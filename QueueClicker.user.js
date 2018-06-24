d6003b719cd2e594b9e3735d29b76d4f26567f06
Kirill Korepanov <kirill.korepanov@ika.de>
2018-06-24 14:58:06 +0200

    Fixed script to work with non-english steam localizations

--- QueueClicker.user.js
+++ QueueClicker.user.js
@@ -2,7 +2,7 @@
 // @name         Steam Sale Queue Clicker
 // @namespace    https://github.com/Xorboo/Steam-Sale-Queue-Clicker
 // @author       Xorboo
-// @version      0.1.2
+// @version      0.2.1
 // @description  Simple clicker for getting trading cards during steam sale. Stops after recieving all cards for the day
 // @downloadURL  https://github.com/Xorboo/Steam-Sale-Queue-Clicker/blob/master/QueueClicker.user.js
 // @updateURL    https://openuserjs.org/meta/Xorboo/Steam_Sale_Queue_Clicker.meta.js
@@ -11,39 +11,39 @@
 // @match        http://store.steampowered.com/*
 // @match        https://store.steampowered.com/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 1ms delay so button will actually be clicked
+    // 10ms delay so button will actually be clicked
     function executeWithDelay(func) {
-        setTimeout(func, 1);
+        setTimeout(func, 10);
     }
 
     // Call "Next in Queue" button
     var nextButton = $('.next_in_queue_content');
     if (nextButton.length) {
-        console.log("Checking next game");
-        executeWithDelay(function() {
-            nextButton.click();
-        });
+        console.log('Checking next game');
+        executeWithDelay(() => { nextButton.click(); });
     }
 
     // Call "Start another queue" button if needed
-    var hintText = $(".discovery_queue_winter_sale_cards_header .subtext").html();
+    var hintText = $('.discovery_queue_winter_sale_cards_header .subtext').html();
     if (hintText) {
-        console.log("Queue completed.");
+        console.log('Queue completed');
 
-        var regExp = new RegExp("You can get (\\d)+ more card(s)? today by continuing to browse your Discovery Queue\\.");
+        // Only works for english localization
+        //var regExp = new RegExp("You can get (\\d)+ more card(s)? today by continuing to browse your Discovery Queue\\.");
+
+        var regExp = new RegExp('(\\d)+');
         if (regExp.test(hintText)) {
-            console.log("Calling next queue to get another card");
-            executeWithDelay(function() {
-                document.querySelector('#refresh_queue_btn').click();
-            });
+            console.log('Calling next queue to get another card');
+            executeWithDelay(() => { $('#refresh_queue_btn').click() });
         }
         else {
-            console.log("All cards recieved, stopping clicker");
+            console.log('All cards recieved, stopping clicker');
         }
     }
 })();
--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Clicks `Next in Queue` and `Start another queue` (only if there are more cards a
  * Get [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) for Chrome or [GreaseMonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey) for Firefox
  * Open userscript source [here](https://openuserjs.org/scripts/Xorboo/Steam_Sale_Queue_Clicker)
  * Click `Install`
- * Go to Steam [`Explore`](http://store.steampowered.com/explore/) page, script will take it from there and stop when all cards for the day are recieved.
+ * Go to Steam [`Explore`](http://store.steampowered.com/explore/) page, start queue, script will take it from there and stop when all cards for the day are recieved.
