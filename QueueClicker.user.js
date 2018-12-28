4733738f8b2a8a98e06cd3a213e646e35e637837
Kirill Korepanov <xorboo@gmail.com>
2018-12-29 00:59:15 +0100

    Automatically start first queue if needed

--- QueueClicker.user.js
+++ QueueClicker.user.js
@@ -13,37 +13,49 @@
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @grant        none
 // ==/UserScript==
+/* globals jQuery, $ */
 
 (function() {
     'use strict';
 
     // 10ms delay so button will actually be clicked
-    function executeWithDelay(func) {
+    const executeWithDelay = function(func) {
         setTimeout(func, 10);
     }
 
-    // Call "Next in Queue" button
-    var nextButton = $('.next_in_queue_content');
-    if (nextButton.length) {
-        console.log('Checking next game');
-        executeWithDelay(() => { nextButton.click(); });
+    const checkButtons = function() {
+        // Call "Begin exploring your queue
+        const startQueueTextVisible = $('#discovery_queue_static');
+        if (startQueueTextVisible.length && startQueueTextVisible.css('display') != 'none') {
+            console.log('Starting queue');
+            $('#discovery_queue_start_link').click();
+        }
+
+        // Call "Next in Queue" button
+        const nextButton = $('.next_in_queue_content');
+        if (nextButton.length) {
+            console.log('Checking next game');
+            executeWithDelay(() => { nextButton.click(); });
+        }
+
+        // Call "Start another queue" button if needed
+        const queueHeader = $('.discovery_queue_winter_sale_cards_header .subtext');
+        if (queueHeader.length) {
+            console.log('Queue completed');
+
+            // Only works for english localization
+            //var regExp = new RegExp("You can get (\\d)+ more card(s)? today by continuing to browse your Discovery Queue\\.");
+
+            const regExp = new RegExp('(\\d)+');
+            if (regExp.test(queueHeader.html())) {
+                console.log('Calling next queue to get another card');
+                executeWithDelay(() => { $('#refresh_queue_btn').click() });
+            }
+            else {
+                console.log('All cards recieved, stopping clicker');
+            }
+        }
     }
 
-    // Call "Start another queue" button if needed
-    var hintText = $('.discovery_queue_winter_sale_cards_header .subtext').html();
-    if (hintText) {
-        console.log('Queue completed');
-
-        // Only works for english localization
-        //var regExp = new RegExp("You can get (\\d)+ more card(s)? today by continuing to browse your Discovery Queue\\.");
-
-        var regExp = new RegExp('(\\d)+');
-        if (regExp.test(hintText)) {
-            console.log('Calling next queue to get another card');
-            executeWithDelay(() => { $('#refresh_queue_btn').click() });
-        }
-        else {
-            console.log('All cards recieved, stopping clicker');
-        }
-    }
+    executeWithDelay(checkButtons);
 })();
